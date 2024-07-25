8392c412967ab316d9eb6e63ff1a9396fb8b277c
Mona Lisa <thisissupersuperdumb@gmail.com>
2024-07-25 20:24:19 +0000

    views and urls

---
+++ phoenixcafe/phoenixcafe/views.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+
+import datetime
+
+# imports from Django
+from django.shortcuts import render
+from django.http import HttpResponse
+
+# This view will return "Welcome to the Phoenix Cafe!" as text
+def welcome(request):
+    return HttpResponse("Welcome to the Phoenix Cafe!")
+def current_datetime(request):
+    now = datetime.datetime.now()
+    html = "<html><body>It is now %s.</body></html>" % now
+    return HttpResponse(html)
